86cd1375a9b198771a474d6bc0a40dbf0930a106
BULUT-Melissa-25006748 <melissa.bulut@etu.univ-amu.fr>
2026-06-13 11:51:04 +0000

    Exercice 5 terminé

--- src/main/java/fr/univ_amu/iut/exercice5/PokemonViewModel.java
+++ src/main/java/fr/univ_amu/iut/exercice5/PokemonViewModel.java
@@ -1,6 +1,7 @@
 package fr.univ_amu.iut.exercice5;
 
 import com.google.inject.Inject;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -37,6 +38,8 @@ public class PokemonViewModel {
     //
     // - pokemons.setAll(service.tousLesPokemons());
     // - resume.bind(Bindings.size(pokemons).asString().concat(" Pokémon"));
+    pokemons.setAll(service.tousLesPokemons());
+    resume.bind(Bindings.size(pokemons).asString().concat(" Pokémon"));
   }
 
   public ObservableList<Pokemon> pokemonsProperty() {
@@ -69,5 +72,19 @@ public class PokemonViewModel {
     //    S'il est déjà présent : publier un statut (sans l'ajouter en double).
     //    S'il n'existe pas : publier un statut "introuvable".
     // Astuce : Optional offre ifPresentOrElse(present, absent).
+    statut.set("");
+    service
+        .chercherParNom(recherche.get())
+        .ifPresentOrElse(
+            pokemonTrouve -> {
+              if (pokemons.contains(pokemonTrouve)) {
+                statut.set("Ce Pokémon est déjà dans votre Pokédex !");
+              } else {
+                // Sinon on l'ajoute et on vide la barre de recherche
+                pokemons.add(pokemonTrouve);
+                recherche.set("");
+              }
+            },
+            () -> statut.set("Pokémon introuvable."));
   }
 }
